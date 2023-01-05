bbcaad9fabfac71ed48804d77ea4f606ed8ccce9
Harrison Goldstein <hgoldstein95@gmail.com>
2023-01-04 20:55:35 -0500

    Update rbt example

--- examples/rbt/test_rbt.ml
+++ examples/rbt/test_rbt.ml
@@ -14,60 +14,54 @@ let invariant = Rbt.invariant ~compare:Int.compare
 let cgen_rbt : (int, int) Rbt.t Crowbar.gen =
   let open Crowbar in
   let open Let_syntax in
-  let loop self n =
-    if n = 0 then return Rbt.E
-    else
-      choose
-        [
-          return Rbt.E;
-          (let%bind rb = choose [ const Rbt.B; const Rbt.R ] in
-           let%bind a = self (n - 1) in
-           let%bind x = int in
-           let%bind vx = int in
-           let%bind b = self (n - 1) in
-           return (Rbt.T (rb, a, x, vx, b)));
-        ]
-  in
-  fix' loop 5
+  fix' ~init:5 (fun self n ->
+      if n = 0 then return Rbt.E
+      else
+        choose
+          [
+            return Rbt.E;
+            (let%bind rb = choose [ const Rbt.B; const Rbt.R ] in
+             let%bind a = self (n - 1) in
+             let%bind x = int in
+             let%bind vx = int in
+             let%bind b = self (n - 1) in
+             return (Rbt.T (rb, a, x, vx, b)));
+          ])
 
 let cgen_rbt_bst : (int, int) Rbt.t Crowbar.gen =
   let open Crowbar in
   let open Let_syntax in
-  let loop self (lo, hi) =
-    if lo > hi then return Rbt.E
-    else
-      choose
-        [
-          return Rbt.E;
-          (let%bind rb = choose [ const Rbt.B; const Rbt.R ] in
-           let%bind x = range ~min:lo hi in
-           let%bind a = self (lo, x - 1) in
-           let%bind vx = int in
-           let%bind b = self (x + 1, hi) in
-           return (Rbt.T (rb, a, x, vx, b)));
-        ]
-  in
-  fix' loop (1, 20)
+  fix' ~init:(1, 20) (fun self (lo, hi) ->
+      if lo > hi then return Rbt.E
+      else
+        choose
+          [
+            return Rbt.E;
+            (let%bind rb = choose [ const Rbt.B; const Rbt.R ] in
+             let%bind x = range ~min:lo hi in
+             let%bind a = self (lo, x - 1) in
+             let%bind vx = int in
+             let%bind b = self (x + 1, hi) in
+             return (Rbt.T (rb, a, x, vx, b)));
+          ])
 
 let cgen_rbt_bst_bal : (int, int) Rbt.t Crowbar.gen =
   let open Crowbar in
   let open Let_syntax in
-  let loop self (prev_c, bh, lo, hi) =
-    if lo > hi || bh = 0 then return Rbt.E
-    else
-      let%bind rb =
-        match prev_c with
-        | Rbt.R -> return Rbt.B
-        | Rbt.B -> choose [ const Rbt.B; const Rbt.R ]
-      in
-      let%bind x = range ~min:lo hi in
-      let%bind a = self (rb, bh - 1, lo, x - 1) in
-      let%bind vx = int in
-      let%bind b = self (rb, bh - 1, x + 1, hi) in
-      return (Rbt.T (rb, a, x, vx, b))
-  in
   let%bind bh = range ~min:1 4 in
-  fix' loop (Rbt.R, bh, 1, 20)
+  fix' ~init:(Rbt.R, bh, 1, 20) (fun self (prev_c, bh, lo, hi) ->
+      if lo > hi || bh = 0 then return Rbt.E
+      else
+        let%bind rb =
+          match prev_c with
+          | Rbt.R -> return Rbt.B
+          | Rbt.B -> choose [ const Rbt.B; const Rbt.R ]
+        in
+        let%bind x = range ~min:lo hi in
+        let%bind a = self (rb, bh - 1, lo, x - 1) in
+        let%bind vx = int in
+        let%bind b = self (rb, bh - 1, x + 1, hi) in
+        return (Rbt.T (rb, a, x, vx, b)))
 
 let crowbar_insert_valid (t, k, v) =
   let open Crowbar in
